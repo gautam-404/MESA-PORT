d1ac379f4b126f500ae56ecf4e146684f2d5e466
gautam-404 <gautam.anuj.1195@gmail.com>
2023-01-15 17:40:46 +0530

    requirements

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('requirements.txt') as f:
 
 
 setup(
-    name='MESA-commando',
+    name='MESAcommando',
     version='1.0',
     packages=find_packages(),
     install_requires=required,
