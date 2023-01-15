14208f511c5641f6ddf77683aa9acf58b80ae4c4
gautam-404 <gautam.anuj.1195@gmail.com>
2023-01-15 06:31:04 +0530

    update

--- example.py
+++ example.py
@@ -1,27 +1,23 @@
 from MESAcommando import  ProjectOps, MesaAccess
 
 ## Use ProjectOps("your_project") for a custom project name
-work = ProjectOps() 
-work.create(overwrite=True, clean=False)         
+work = ProjectOps()          
 
-w = MesaAccess()
-w["create_pre_main_sequence_model"] = True
+## Use boolean arguments 'overwrite' and 'clean' to work on existing projects
+work.create()   
+work.make()           
 
-# ## Use boolean arguments 'overwrite' and 'clean' to work on existing projects
-# work.create()   
-# work.make()           
+work.loadProjInlist("/path/to/inlist")
+# work.loadPGstarInlist("/path/to/inlist")
 
-# work.loadProjInlist("/path/to/inlist")
-# # work.loadPGstarInlist("/path/to/inlist")
+object = MesaAccess()
+object["initial_mass"] = 5
 
-# object = MesaAccess()
-# object["initial_mass"] = 5
+## Use argument silent=True (False by default) for a silent run, terminal output is redirected to runlog
+work.run()              
 
-# ## Use argument silent=True (False by default) for a silent run, terminal output is redirected to runlog
-# work.run()              
-
-# ## arg silent is False by default
-# work.rerun("x450", silent=True)      
+## arg silent is False by default
+work.rerun("x450", silent=True)      
  
-# ## Clean the project
-# work.clean()              
+## Clean the project
+work.clean()              
