9f70f9fb16ed0f6bbcaf7b58cccc7784de32bf26
gautam-404 <gautam.anuj.1195@gmail.com>
2023-01-15 17:40:14 +0530

    requirements

--- setup.py
+++ setup.py
@@ -1,10 +1,14 @@
-import pip
 from setuptools import setup,find_packages
+
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
+
 
 setup(
     name='MESA-commando',
     version='1.0',
     packages=find_packages(),
+    install_requires=required,
     url='https://github.com/gautam-404/MESAcommando.git',
     author='gautam-404',
     description='Be a MESA commander!',
