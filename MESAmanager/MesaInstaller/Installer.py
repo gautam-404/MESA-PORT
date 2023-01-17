f6569f4a77e92a3eca74293d119ef8a8aa541310
gautam-404 <gautam.anuj.1195@gmail.com>
2023-01-17 21:11:36 +0530

    installer

--- MESAmanager/MesaInstaller/Installer.py
+++ MESAmanager/MesaInstaller/Installer.py
@@ -1,6 +1,7 @@
-import os
+import os, shutil, sys
 import platform
-import subprocess
+import tarfile
+import zipfile
 
 import requests
 from alive_progress import alive_bar
@@ -11,6 +12,10 @@ console = Console()
 class Installer:
     def __init__(self, version='', parent_directory=''):
         self.install(version, parent_directory)
+        return
+
+
+
 
     def whichos(self):
         if "macOS" in platform.platform():
@@ -20,17 +25,26 @@ class Installer:
         else:
             raise OSError(f"OS {platform.platform()} not compatible.")
 
+
+
+
     def choose_directory(self, directory=''):
         while not os.path.exists(directory):
             directory = input("Input path to a directory for installation...")
             if os.path.exists(directory):
                 print(f"MESA SDK and MESA will be installed at path: {directory}/software/")
                 if not os.path.exists(directory+"/software"):
-                        os.mkdir(directory+"/software")
+                    os.mkdir(directory+"/software")
                 return os.path.abspath(directory+"/software")
             else:
                 print("Could not find the specified directory. Please try again.")
                 directory = ''
+        if not os.path.exists(directory+"/software"):
+            os.mkdir(directory+"/software")
+        return os.path.abspath(directory+"/software")
+
+
+
 
     def choose_ver(self, ver=''):
         versions = ["latest", "22.11.1", "22.05.1", "21.12.1", "15140", "12778"]
@@ -41,6 +55,8 @@ class Installer:
             if ver not in versions:
                 print("Not recognised, try again.")
         return ver
+
+
 
     def prep_urls(self, ver):
         if self.whichos() == "Linux":
@@ -87,49 +103,80 @@ class Installer:
         check_n_download(sdk_tar, sdk_url)
 
         print("Downloading MESA...")
-        mesa_tar = directory+"/"+mesa_url.split('/')[-1]
-        check_n_download(mesa_tar, mesa_url)
-        return sdk_tar, mesa_tar
+        mesa_zip = directory+"/"+mesa_url.split('/')[-1]
+        check_n_download(mesa_zip, mesa_url)
+        return sdk_tar, mesa_zip
+
+
+
+    def install_pre_reqs(self):
+        if self.whichos() == "Linux":
+            os.system('''
+                    sudo apt-get update
+                    sudo apt-get install -yq build-essential wget curl binutils make perl libx11-6 libx11-dev zlib1g zlib1g-dev tcsh \\
+                        libX11 libX11-devel zlib zlib-devel \\
+                        zlib-dev libc6-dev \\
+                        glibc
+                        ''')
+
+
+    def print_env_vars(self, directory, mesa_dir):
+        source_this=f'''
+        export MESASDK_ROOT={directory}/mesasdk
+        source $MESASDK_ROOT/bin/mesasdk_init.sh
+        export MESA_DIR={mesa_dir}
+        export OMP_NUM_THREADS=2
+        export GYRE_directory=$MESA_DIR/gyre/gyre
+        '''
+        print("Please add the following to the appropriate shell start-up file (~/.*rc or ~/.*profile):")
+        print(source_this)
+
+
+
+
+    def set_env_vars(self, directory, mesa_dir):
+        os.environ['MESASDK_ROOT'] = directory+"/mesasdk"
+        os.system(f"export MESASDK_ROOT={directory}/mesasdk && source $MESASDK_ROOT/bin/mesasdk_init.sh")
+        os.environ['MESA_DIR'] = mesa_dir
+        os.environ['OMP_NUM_THREADS'] = "2"
+        os.environ['GYRE_directory'] = mesa_dir+"/gyre/gyre"
+
+
+
+
+    def extract_mesa(self, directory, sdk_tar, mesa_zip):
+        with console.status("Extracting MESA SDK...", spinner="moon"):
+            with tarfile.open(sdk_tar, 'r:gz') as tarball:
+                if os.path.exists(f'{directory}/mesasdk'):
+                    shutil.rmtree(f'{directory}/mesasdk')
+                tarball.extractall(f'{directory}/')
+            # os.remove(sdk_tar)
+        print("MESA SDK extraction complete.")
+        with console.status("Extracting MESA...", spinner="moon"):
+            with zipfile.ZipFile(mesa_zip, 'r') as zip_ref:
+                zip_ref.extractall(f'{directory}/')
+            # os.remove(mesa_zip)
+        print("MESA extraction complete.")
+
+
+
 
     def install(self, ver='', directory=''):
         ver = self.choose_ver(ver)
         directory = self.choose_directory(directory)
         sdk_url, mesa_url = self.prep_urls(ver)
-        sdk_tar, mesa_tar = self.download(directory, sdk_url, mesa_url)
+        sdk_tar, mesa_zip = self.download(directory, sdk_url, mesa_url)
+        mesa_dir = directory+'/'+mesa_zip.split('/')[-1][0:-4]
 
+        self.install_pre_reqs()
+        self.extract_mesa(directory, sdk_tar, mesa_zip)
+        self.set_env_vars(directory, mesa_dir)
 
-        with console.status("Installing MESA SDK...", spinner="moon"):
-            subprocess.call(f"tar xvfz {sdk_tar} -C {directory}/")
-            # os.remove(sdk_tar)
-            subprocess.call(f"export MESASDK_ROOT={directory}/mesasdk")
-            subprocess.call("source $MESASDK_ROOT/bin/mesasdk_init.sh")
-        print("MESA SDK installation complete.")
-
-        with console.status("Installing MESA...", spinner="moon"):
-            subprocess.call(f"unzip {mesa_tar} -d {directory}/")
-            # os.remove(mesa_tar)
-    #         suprocess.call(f"cd {directory}/software/")
-    #         cd $DIR/mesa-r15140
-    # ./install
-            subprocess.call(f"export MESA_directory={directory}/{mesa_url.split('/')[-1]}")
-            subprocess.call("export OMP_NUM_THREADS=2")
-        print("MESA installation complete.")
-
-        with console.status("Installing GYRE...", spinner="moon"):
-            subprocess.call("export GYRE_directory=$MESA_directory/gyre/gyre")
-            subprocess.call("cd $GYRE_directory; make")
-        print("GYRE installation complete.")
-        
-
-        print("Please add the following to the appropriate shell start-up file (~/.*rc or ~/.*profile):")
-        source_this=f'''
-        export MESASDK_ROOT={directory}/mesasdk
-        source $MESASDK_ROOT/bin/mesasdk_init.sh
-        export MESA_directory={directory}/{mesa_url.split('/')[-1]}
-        export OMP_NUM_THREADS=2
-        export GYRE_directory=$MESA_directory/gyre/gyre
-        '''
-        print(source_this)
-
-        print("Installation complete!")
+        with console.status("Installing...", spinner="moon"):
+            if os.system("cd $MESA_DIR && chmod +x clean && chmod +x install && ./clean && ./install") != 0:
+                print("Installation failed.")
+            if os.system("cd $GYRE_DIR && chmod +x make && make") != 0:
+                print("Installation failed.")
+        print("Installation complete.")
+        self.print_env_vars(directory, mesa_dir)
 
