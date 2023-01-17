0af470aae21460b807760f5053c7e55e7a1f4792
Anuj Gautam <45739450+gautam-404@users.noreply.github.com>
2023-01-17 09:54:16 +0000

    installer

--- MESAmanager/MesaInstaller/Installer.py
+++ MESAmanager/MesaInstaller/Installer.py
@@ -2,8 +2,8 @@ import platform, subprocess, os
 import requests
 
 class Installer:
-    def __init__(self, version='', parent_dir=''):
-        self.install(version, parent_dir)
+    def __init__(self, version='', parent_directory=''):
+        self.install(version, parent_directory)
 
     def whichos(self):
         if "macOS" in platform.platform():
@@ -13,15 +13,15 @@ class Installer:
         else:
             raise OSError(f"OS {platform.platform()} not compatible.")
 
-    def choose_dir(self, dir=''):
-        while not os.path.exists(dir):
-            dir = input("Input path to a directory for installation...")
-            if os.path.exists(dir):
-                print(f"MESA SDK and MESA will be installed at {dir}/software/")
-                return dir
+    def choose_directory(self, directory=''):
+        while not os.path.exists(directory):
+            directory = input("Input path to a directoryectory for installation...")
+            if os.path.exists(directory):
+                print(f"MESA SDK and MESA will be installed at {directory}/software/")
+                return directory
             else:
-                print(f"Could not find the specified directory. Please try again.")
-                dir = ''
+                print("Could not find the specified directoryectory. Please try again.")
+                directory = ''
 
     def choose_ver(self, ver=''):
         versions = ["latest", "22.11.1", "22.05.1", "21.12.1", "15140", "12778"]
@@ -55,55 +55,55 @@ class Installer:
                 mesa_url = "https://zenodo.org/record/7319739/files/mesa-r12778.zip"
         return sdk_url, mesa_url
 
-    def download(self, dir, sdk_url, mesa_url):
-        os.mkdir(dir+"/software")
+    def download(self, directory, sdk_url, mesa_url):
+        os.mkdir(directory+"/software")
 
-        sdk_tar = dir+"/software/"+sdk_url.split('/')[-1]
+        sdk_tar = directory+"/software/"+sdk_url.split('/')[-1]
         response = requests.get(sdk_url, stream=True, timeout=10)
         try:
             if response.status_code == 200:
-                with open(sdk_tar, 'wb') as f:
-                    f.write(response.raw.read())
+                with open(sdk_tar, 'wb') as file:
+                    file.write(response.raw.read())
         except Exception:
             print("Could not download MESA SDK. Please try again later.")
 
-        mesa_tar = dir+"/software/"+mesa_url.split('/')[-1]
+        mesa_tar = directory+"/software/"+mesa_url.split('/')[-1]
         response = requests.get(mesa_url, stream=True)
         try:
             if response.status_code == 200:
-                with open(mesa_tar, 'wb') as f:
-                    f.write(response.raw.read())
+                with open(mesa_tar, 'wb') as file:
+                    file.write(response.raw.read())
         except Exception:
             print("Could not download MESA. Please try again later.")
         return sdk_tar, mesa_tar
 
-    def install(self, ver='', dir=''):
+    def install(self, ver='', directory=''):
         ver = self.choose_ver(ver)
-        dir = self.choose_dir(dir)
+        directory = self.choose_directory(directory)
         sdk_url, mesa_url = self.prep_urls(ver)
-        sdk_tar, mesa_tar = self.download(dir, sdk_url, mesa_url)
+        sdk_tar, mesa_tar = self.download(directory, sdk_url, mesa_url)
 
-        subprocess.call(f"tar xvfz {sdk_tar} -C {dir}/software/")
+        subprocess.call(f"tar xvfz {sdk_tar} -C {directory}/software/")
         os.remove(sdk_tar)
-        subprocess.call(f"export MESASDK_ROOT={dir}/software/mesasdk")
+        subprocess.call(f"export MESASDK_ROOT={directory}/software/mesasdk")
         subprocess.call("source $MESASDK_ROOT/bin/mesasdk_init.sh")
 
-        subprocess.call(f"unzip {mesa_tar} -d {dir}/software/")
+        subprocess.call(f"unzip {mesa_tar} -d {directory}/software/")
         os.remove(mesa_tar)
-        subprocess.call(f"export MESA_DIR={dir}/software/{mesa_url.split('/')[-1]}")
+        subprocess.call(f"export MESA_directory={directory}/software/{mesa_url.split('/')[-1]}")
         subprocess.call("export OMP_NUM_THREADS=2")
 
-        subprocess.call("export GYRE_DIR=$MESA_DIR/gyre/gyre")
-        subprocess.call("cd $GYRE_DIR; make")
+        subprocess.call("export GYRE_directory=$MESA_directory/gyre/gyre")
+        subprocess.call("cd $GYRE_directory; make")
         
 
         print("Please add the following to the appropriate shell start-up file (~/.*rc or ~/.*profile):")
         source_this=f'''
-        export MESASDK_ROOT={dir}/software/mesasdk
+        export MESASDK_ROOT={directory}/software/mesasdk
         source $MESASDK_ROOT/bin/mesasdk_init.sh
-        export MESA_DIR={dir}/software/{mesa_url.split('/')[-1]}
+        export MESA_directory={directory}/software/{mesa_url.split('/')[-1]}
         export OMP_NUM_THREADS=2
-        export GYRE_DIR=$MESA_DIR/gyre/gyre
+        export GYRE_directory=$MESA_directory/gyre/gyre
         '''
         print(source_this)
 
