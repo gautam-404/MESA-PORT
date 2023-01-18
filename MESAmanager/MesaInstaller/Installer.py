8c7a00f7ae0d023acec83784346de3fbbbae289c
gautam-404 <gautam.anuj.1195@gmail.com>
2023-01-19 04:43:25 +0530

    MESA SDK init check. MESA build check

--- MESAmanager/MesaInstaller/Installer.py
+++ MESAmanager/MesaInstaller/Installer.py
@@ -192,7 +192,7 @@ class Installer:
                 elif "macOS" in self.ostype:
                     sdk_dir = '/Applications/mesasdk'
 
-                if subprocess.call(f"/bin/bash -c \"export MESASDK_ROOT={self.directory}/mesasdk && \
+                if subprocess.call(f"/bin/bash -c \"export MESASDK_ROOT={sdk_dir} && \
                             source $MESASDK_ROOT/bin/mesasdk_init.sh && gfortran --version\"",
                             shell=True, stdout=logfile, stderr=logfile) != 0:
                     raise Exception("MESA SDK initialization failed. \
