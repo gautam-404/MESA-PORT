caf7e2f3a0801ac95bd415e6660e158fdfddae76
gautam-404 <gautam.anuj.1195@gmail.com>
2023-01-30 04:01:11 +0530

    gyre test

--- MESAcontroller/MesaProjManager/ops_helper.py
+++ MESAcontroller/MesaProjManager/ops_helper.py
@@ -3,13 +3,15 @@ import shlex
 import sys
 from rich import status
 
+from ..MesaFileHandler.support import *
+
 def check_exists(exists, projName):
         """Checks if the project exists."""
         if not exists:
             raise FileNotFoundError(f"Project '{projName}' does not exist. Please create it first.")
 
 
-def run_subprocess(commands, dir, silent=False, runlog='', status=status.Status("Running...")):
+def run_subprocess(commands, dir, silent=False, runlog='', status=status.Status("Running..."), gyre=False):
     """Runs a subprocess.
 
     Args:
@@ -27,22 +29,19 @@ def run_subprocess(commands, dir, silent=False, runlog='', status=status.Status(
         with open(runlog, "a+") as file:
             for outline in proc.stdout:
                 file.write(outline)
-                step, age = process_outline(outline, step)
                 if silent is False:
                     sys.stdout.write(outline)
-                elif age is not None:
-                    if age < 1/365:
-                        status.update(status=f"[b i cyan3]Running....[/b i cyan3]\n[b]Current age:\
-                             [cyan]{age*365*24:.4f}[/cyan] hours")
-                    elif 1/365 < age < 1:
-                        status.update(status=f"[b i cyan3]Running....[/b i cyan3]\n[b]Current age:\
-                             [cyan]{age*365:.4f}[/cyan] days")
-                    elif 1 < age < 1000:
-                        status.update(status=f"[b i cyan3]Running....[/b i cyan3]\n[b]Current age:\
-                             [cyan]{age:.3f}[/cyan] years")
-                    else:
-                        status.update(status=f"[b i cyan3]Running....[/b i cyan3]\n[b]Current age:\
-                             [cyan]{age:.3e}[/cyan] years")
+                elif not gyre:
+                    step, age = process_outline(outline, step)
+                    if age is not None:
+                        if age < 1/365:
+                            status.update(status=f"[b i cyan3]Running....[/b i cyan3]\n[b]Current age: [cyan]{age*365*24:.4f}[/cyan] hours")
+                        elif 1/365 < age < 1:
+                            status.update(status=f"[b i cyan3]Running....[/b i cyan3]\n[b]Current age: [cyan]{age*365:.4f}[/cyan] days")
+                        elif 1 < age < 1000:
+                            status.update(status=f"[b i cyan3]Running....[/b i cyan3]\n[b]Current age: [cyan]{age:.3f}[/cyan] years")
+                        else:
+                            status.update(status=f"[b i cyan3]Running....[/b i cyan3]\n[b]Current age: [cyan]{age:.3e}[/cyan] years")
             for errline in proc.stderr:
                 file.write(errline)
                 sys.stdout.write(errline)
@@ -66,3 +65,42 @@ def process_outline(outline, step):
         return step, None
     except:
         return step, None
+
+
+def writetoGyreFile(projectDir, parameter, value):
+    """Writes the parameter and its value to the inlist file.
+
+    Args:
+        filename (str): The path to the inlist file.
+        parameter (str): The parameter to be written.
+        value (str): The value of the parameter to be written.
+        inlistDict (dict): A dictionary with all the parameters and their values.
+        defaultsDict (dict): A dictionary with all the parameters and their values.
+        sections (list): A list with the sections of the inlist file.
+    """    
+    filename = "gyre.in"
+    default_section = "model"
+    this_section = False
+    with cd(projectDir):
+        with open(filename, "r") as file:
+            lines = file.readlines()
+        with open(filename, "w+") as f:
+            indent = "    "
+            for line in lines:
+                edited = False
+                if "&"+default_section in line:
+                    this_section = True
+                if this_section:
+                    if parameter in line:
+                        f.write(line.replace(line.split("=")[1], f" {value}    ! Changed\n"))
+                        edited = True
+                        this_section = False
+                    elif line[0] == "/":
+                        f.write(indent)
+                        f.write(f"{parameter} = {value}    ! Added\n")
+                        f.write("/")
+                        edited = True
+                        this_section = False
+                if not edited:
+                    f.write(line)
+            f.write("\n")   
