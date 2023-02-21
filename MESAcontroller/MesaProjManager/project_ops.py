616b863c198035d056ca8a14ec7c0e4ba281ee7b
gautam-404 <gautam.anuj.1195@gmail.com>
2023-02-21 13:06:42 +0530

    test

--- MESAcontroller/MesaProjManager/project_ops.py
+++ MESAcontroller/MesaProjManager/project_ops.py
@@ -316,7 +316,7 @@ class ProjectOps:
                         task = progressbar.add_task("[b i cyan3]Running GYRE...", total=len(files))
                         n_processes = (os.cpu_count()//int(os.environ['OMP_NUM_THREADS']))
                         print(os.cpu_count(), os.environ['OMP_NUM_THREADS'], n_processes)
-                        with mp.Pool(n_processes, initializer=os.system, initargs="sleep 0.1") as pool:
+                        with mp.Pool(n_processes, initializer=os.system, initargs=("sleep 0.1",)) as pool:
                             gyre_in = os.path.abspath(gyre_in)
                             args = zip(repeat(f'{gyre_ex} gyre.in'), repeat(LOGS_dir),
                                     repeat(silent), repeat(runlog),
