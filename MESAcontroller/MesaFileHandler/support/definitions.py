1434ebda9938646b05a64cc6c2f688e5216b16b1
gautam-404 <gautam.anuj.1195@gmail.com>
2023-06-11 02:12:07 +1000

    binary

--- MESAcontroller/MesaFileHandler/support/definitions.py
+++ MESAcontroller/MesaFileHandler/support/definitions.py
@@ -3,10 +3,10 @@ externalFileParameters_star = ["extra_star_job_inlist1_name","extra_controls_inl
 defaultsFileNames_star = {"star_job": "star_job.defaults", "eos": "eos.defaults", "kap": "kap.defaults",
                             "controls": "controls.defaults", "pgstar": "pgstar.defaults"}
 
-sections_binary = ["binary_job", "binary_controls", "pgbinary"]
+sections_binary = ["binary_job", "binary_controls"]
 externalFileParameters_binary = ["extra_binary_job_inlist1_name","extra_binary_controls_inlist1_name","extra_pgbinary_inlist1_name",
                             "extra_star_job_inlist1_name","extra_controls_inlist1_name","extra_pgstar_inlist1_name"]
-defaultsFileNames_binary = {"binary_job": "binary_job.defaults", "binary_controls": "binary_controls.defaults", "pgbinary": "pgbinary.defaults"}
+defaultsFileNames_binary = {"binary_job": "binary_job.defaults", "binary_controls": "binary_controls.defaults"}
 
 sections_astero = ["star_job", "eos", "kap", "controls", "pgstar", "astero_search_controls", "astero_pgstar_controls"]
 externalFileParameters_astero = ["extra_star_job_inlist1_name","extra_controls_inlist1_name","extra_pgstar_inlist1_name"]
