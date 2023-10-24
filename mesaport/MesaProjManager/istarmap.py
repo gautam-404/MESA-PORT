63f0f4ec38f4a750d24c8eb8536246ffe4d381c8
gautam-404 <gautam.anuj.1195@gmail.com>
2023-10-24 15:41:31 +1000

    pass custom environment to GYRE and MESA runs

--- mesaport/MesaProjManager/istarmap.py
+++ mesaport/MesaProjManager/istarmap.py
@@ -3,7 +3,35 @@ import multiprocessing.pool as mpp
 
 
 def istarmap(self, func, iterable, chunksize=1):
-    """starmap-version of imap
+    """
+    starmap-version of imap
+
+    Parameters
+    ----------
+    func : callable
+        The function to apply to the items of the iterable.
+    iterable : iterable
+        The iterable to apply the function to.
+    chunksize : int, optional
+        The number of items to send to the worker processes at once.
+        Default is 1.
+
+    Returns
+    -------
+    generator
+        A generator that yields the results of applying the function to
+        the items of the iterable.
+
+    Raises
+    ------
+    ValueError
+        If chunksize is less than 1.
+
+    Notes
+    -----
+    This method is similar to the built-in `itertools.starmap`, but
+    uses a pool of worker processes to apply the function in parallel.
+
     """
     self._check_running()
     if chunksize < 1:
