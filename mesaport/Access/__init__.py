2cac31fb23030fc0bd50a9706d56c00484e7255a
gautam-404 <gautam.anuj.1195@gmail.com>
2023-11-11 18:54:39 +1000

    init

--- mesaport/Access/__init__.py
+++ mesaport/Access/__init__.py
@@ -1,3 +1,11 @@
+"""
+This module provides access to the MESA inlists, GYRE input files and other files.
+
+Sub-submodules:
+    MesaAccess: Provides access to the MESA inlists. Also allows loading of Inlists, History and Profile columns files.
+    GyreAccess: Provides access to the GYRE input files.
+"""
+
 from .mesa_access import MesaAccess
 from .gyre_access import GyreAccess
 from .envhandler import MesaEnvironmentHandler
